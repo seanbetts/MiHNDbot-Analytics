55a5d314018e540c4046b83e44a0c3fd3c20a203
Sean <seanbetts@gmail.com>
2019-08-26 22:34:51 +0100

    oktaTest6

--- js/index.js
+++ js/index.js
@@ -1912,6 +1912,51 @@ function openPage (pageName, elmnt) {
   elmnt.style.backgroundColor = '#FFFFFF'
 }
 
+// Use the Okta widget to power authentication!
+var oktaSignIn = new OktaSignIn({
+  baseUrl: "https://dev-512183.okta.com",
+  clientId: "0oa17c2rrqLjoUuzE357",
+  authParams: {
+    issuer: "https://dev-512183.okta.com/oauth2/default",
+    responseType: ['token', 'id_token'],
+    display: 'page'
+  }
+});
+    
+if (oktaSignIn.token.hasTokensInUrl()) {
+  oktaSignIn.token.parseTokensFromUrl(
+    // If we get here, the user just logged in.
+    function success(res) {
+      var accessToken = res[0];
+      var idToken = res[1]
+    
+      oktaSignIn.tokenManager.add('accessToken', accessToken);
+      oktaSignIn.tokenManager.add('idToken', idToken);
+    
+      window.location.hash='';
+      document.getElementById("messageBox").innerHTML = "Hello, " + idToken.claims.email + "! You just logged in! :)";
+    },
+    function error(err) {
+      console.error(err);
+    }
+  );
+} else {
+  oktaSignIn.session.get(function (res) {
+    // If we get here, the user is already signed in.
+    if (res.status === 'ACTIVE') {
+      document.getElementById("messageBox").innerHTML = "Hello, " + res.login + "! You are *still* logged in! :)";
+      return;
+    }
+    oktaSignIn.renderEl(
+      { el: '#okta-login-container' },
+      function success(res) {},
+      function error(err) {
+        console.error(err);
+      }
+    );
+  });
+}
+
 // Set Chart.js color scheme
 var chartColorScheme = 'brewer.Blues3'
 
